7e135264bfa58df8229952ee21dbf80422b57ac3
cheshari-pearl <chesharipearl@gmail.com>
2024-04-06 17:52:06 +0300

    work on heaps

---
+++ heaps.py
@@ -0,0 +1,20 @@
+
+import heapq
+
+data = [10, 20, 43, 1, 2, 65, 17, 44, 2, 3,1 ]
+heapq.heapify(data)
+
+print(data)
+
+
+print(heapq.heappop(data))
+
+print(data)
+
+print(heapq.heappush(data, 6))
+print(data)
+
+
+
+
+
